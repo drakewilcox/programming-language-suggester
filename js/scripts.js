d6eec73a6080bac69e423582fdaa292b2544b17b
Drake Wilcox <drakewilcox@gmail.com>
2020-01-17 14:44:59 -0800

    add branching and condition statements to make all questions values add up to different responses when submit button event occurs.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,25 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var quest1 = parseInt($("input:radio[name=quest1]:checked").val());
     var quest2 = parseInt($("input:radio[name=quest2]:checked").val());
-    var quest3 = $("input:radio[name=quest3]:checked").val();
-    var quest4 = $("input:radio[name=quest4]:checked").val();
-    var quest5 = $("input:radio[name=quest5]:checked").val();
-    if (quest1 + quest2 === 4) {
+    var quest3 = parseInt($("input:radio[name=quest3]:checked").val());
+    var quest4 = parseInt($("input:radio[name=quest4]:checked").val());
+    var quest5 = parseInt($("input:radio[name=quest5]:checked").val());
+
+
+    if (quest1 + quest2 + quest3 + quest4 + quest5 <= 8) {
       $(".csharp").show();
+      $(".javascript").hide();
+      $(".swift").hide();
     }
-    else if (quest1 + quest2 === 2) {
+    else if (quest1 + quest2 + quest3 + quest4 + quest5 <= 11) {
+      $(".csharp").hide();
       $(".javascript").show();
+      $(".swift").hide();
+    }
+    else if (quest1 + quest2 + quest3 + quest4 + quest5 <= 15) {
+      $(".csharp").hide();
+      $(".javascript").hide();
+      $(".swift").show();
     }
     event.preventDefault();
   });
