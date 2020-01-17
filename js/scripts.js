c79e60f09d01c12d548bc20c1bbc8d6905712206
Drake Wilcox <drakewilcox@gmail.com>
2020-01-17 13:49:46 -0800

    add functions to scripts.js that change value of quest1 and quest2 to numeric value, and add branches that add the values to dispay different results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-  $("formOne").submit(function(event){
-    var quest1 = $("input:radio[name=quest1]:checked").val();
-    var quest2 = $("input:radio[name=quest2]:checked").val();
+  $("#formOne").submit(function(event) {
+    var quest1 = parseInt($("input:radio[name=quest1]:checked").val());
+    var quest2 = parseInt($("input:radio[name=quest2]:checked").val());
     var quest3 = $("input:radio[name=quest3]:checked").val();
     var quest4 = $("input:radio[name=quest4]:checked").val();
     var quest5 = $("input:radio[name=quest5]:checked").val();
-
-
+    if (quest1 + quest2 === 4) {
+      $(".csharp").show();
+    }
+    else if (quest1 + quest2 === 2) {
+      $(".javascript").show();
+    }
+    event.preventDefault();
   });
 });
